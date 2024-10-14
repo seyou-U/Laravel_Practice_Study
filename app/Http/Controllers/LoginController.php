956c4abc531b06fec14e040fbe0393613741e5a1
seyou-U <90487620+seyou-U@users.noreply.github.com>
2024-10-14 13:51:48 +0900

    [add] ユーザー登録機能を追加

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index(): View
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->only('email', 'password');
+
+        // attemptメソッドはユーザーの認証試行を行うために使用されるメソッド
+        if (Auth::attempt($credentials)) {
+            // セッションIDの再発行を行う
+            $request->session()->regenerate();
+
+            // intendedメソッドは、ログイン後元々アクセスしようとしていたページに遷移するためのメソッド
+            return redirect()->intended('home');
+        }
+
+        return back()->withErrors([
+            'message' => 'メールアドレスまたはパスワードが正しくありません。',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        // セッションを再生成し、セッションからのデータを削除する(セッションの無効化)
+        $request->session()->invalidate();
+
+        // ログアウトした状態でセッション固定攻撃を防ぐ策として、新しいセッショントークンを生成し前のトークンが使用される
+        $request->session()->regenerate();
+
+        return redirect('home');
+    }
+}
