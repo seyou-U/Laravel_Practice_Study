b498956fac2c0e2f3a4337ed68afa364aa4fd15f
seyou-U <90487620+seyou-U@users.noreply.github.com>
2024-10-28 17:54:54 +0900

    [add] バリデーションの実装、検証、バリデーションルールの確認について

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Request as FacadesRequest;
 
 class RegisterController extends Controller
 {
@@ -14,6 +15,7 @@ class RegisterController extends Controller
         return view('regist.register');
     }
 
+    // メソッドインジェクションを用いることでRequestオブジェクトを用いた参照パターンを実装
     public function store(Request $request)
     {
         // [memo] confirmedは別のフィールドと一致しているかどうかのチェック
@@ -28,6 +30,29 @@ class RegisterController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+
+        // リクエストの内容をファザードで記述する方法
+        // $user = User::create([
+        //     'name' => FacadesRequest::get('name'),
+        //     'email' => FacadesRequest::get('email'),
+        //     'password' => Hash::make($request->password),
+        // ]);
+
+        // 特定の入力値のみを取得する場合は
+        // $inputs = FacadesRequest::only(['name', 'age']);
+        // $name = $inputs['name'];
+
+
+        // Requestオブジェクト
+        // アップロードのファイルを取得したい場合はfileメソッドを用いる
+        // $file = FacadesRequest::file('material');
+        // $content = file_get_contents($file->getRealPath());
+
+        // JSONの扱いについて
+        // JSONはgetメソッドもしくはjsonメソッドを用いて取得することができる。
+        // $request_get = $request->get('nested');
+        // $request_json = $request->json('nested');
 
         // 会員登録時のメール送信イベントの着火
         event(new Registered($user));
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRegisterRequest;
 use App\Models\User;
 use App\Services\UserPurchaseService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -22,6 +24,36 @@ class UserController extends Controller
         $result =  $this->service->retrievePurchase($request->get('id'));
 
         return view('user.show', compact('result'));
+    }
+
+    // 引数でUserRegisterRequestクラスのインスタンスを渡す
+    public function register(UserRegisterRequest $request)
+    {
+        // UserRegisterRequestを引数にすることでここまでに来るまでにバリデーション判定が行われている
+
+        // 全ての入力値を取得し、$inputに保存する
+        $inputs = $request->all();
+
+        // バリデーションのルールを定義する
+        // $rules = [
+        //     'name' => 'required',
+        //     'age' => 'integer',
+        // ];
+
+        // バリデーションの実行.エラーの場合は直前の画面にリダイレクトする
+        // $this->validate($request, $rules);
+        // // ここにバリデーション通過後の処理
+        // $name = $request->get('name');
+
+
+        // バリデータクラスのインスタンスを取得する。直前の画面ではなく特定の画面に遷移させたい場合に使用する
+        // $validate = Validator::make($inputs, $rules);
+
+        // if ($validate->fails()) {
+        //     // 値エラーの場合の処理
+        // }
+
+        // バリデーション追加後の処理について記述する
     }
 
     public function store(Request $request): RedirectResponse
