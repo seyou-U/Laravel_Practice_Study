3fa09ff502f3b6f2968abadfcefc3c0e0eca1861
seyou-U <90487620+seyou-U@users.noreply.github.com>
2024-11-02 15:52:49 +0900

    [add] モデルの追加およびテストデータ追加のためのseederとfactoryを作成、追加し実行

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Publisher;
 use App\Models\User;
+use Database\Factories\PublisherFactory;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +17,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // seederクラスはcallメソッドを用いて呼び出す
+        // $this->call(AuthorsTableSeeder::class);
+
+        // Factoryクラスはcreateメソッドを用いることで作成することができる
+        Publisher::factory(50)->create();
     }
 }
