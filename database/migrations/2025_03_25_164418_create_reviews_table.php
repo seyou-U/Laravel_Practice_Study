47251cc4b4fa4feebd2ddf664ecd11853ba1cd6b
seyou-U <90487620+seyou-U@users.noreply.github.com>
2025-03-31 14:57:20 +0900

    [add] 口コミ登録機能追加に伴いマイグレーションファイルとモデルの追加

---
+++ database/migrations/2025_03_25_164418_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('user_id', false);
+            $table->text('title');
+            $table->text('content');
+            $table->foreign('user_id')->references('id')
+                ->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
