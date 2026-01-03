e4331d9b82cb68d14cee6b4370267df2b928c3d1
seyou-U <rikakorink54t@yahoo.co.jp>
2026-01-03 12:01:59 +0900

    [add] 途中までのアップデート

--- app/Http/Controllers/Register/RegisterActionController.php
+++ app/Http/Controllers/Register/RegisterActionController.php
@@ -3,18 +3,39 @@
 namespace App\Http\Controllers\Register;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\RegisterReviewProviderInterface;
+use Illuminate\Events\Dispatcher;
 use Illuminate\Http\RedirectResponse;
-use Laravel\Socialite\Contracts\Factory;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Contracts\Factory as SocialiteFactory;
 
 // 外部サービス認証ページへのリダイレクトするクラス
 final class RegisterActionController extends Controller
 {
-    public function __invoke(Factory $factory): RedirectResponse
+    private $provider;
+    private $dispatcher;
+    private $socialite;
+
+    // データベース登録とEvent発火に伴うクラスのインスタンスが渡される
+    public function __construct(
+        RegisterReviewProviderInterface $provider,
+        Dispatcher $dispatcher,
+        SocialiteFactory $socialite
+    ) {
+        $this->provider = $provider;
+        $this->dispatcher = $dispatcher;
+        $this->socialite = $socialite;
+    }
+    public function __invoke(Request $request): RedirectResponse
     {
         // driverメソッドで外部サービスを指定する
-        return $factory->driver('github')->redirect();
+        // return $factory->driver('github')->redirect();
+        $driver = $request->string('provider', 'github')->toString();
 
         // Amazonドライバを利用する場合
         // return $factory->driver('amazon')->redirect();
+
+        $created = now()->toDateTimeString();
+        return $this->socialite->driver($driver)->redirect();
     }
 }
