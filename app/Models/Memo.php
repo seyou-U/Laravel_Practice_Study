dd229bb0ad0f0f417bb0bffbb4fe6293c083fa0b
seyou-U <rikakorink54t@yahoo.co.jp>
2026-01-09 18:00:27 +0900

    [add] アクセサとミューテタの追加

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Memo extends Model
 {
     use HasFactory;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -17,4 +20,41 @@ class Memo extends Model
         'title',
         'content',
     ];
+
+    // JSONに含めたい属性の追加
+    protected $appends = ['content_excerpt'];
+
+    protected $casts = [
+        'content' => 'encrypted',
+    ];
+
+    /**
+     * Title取得、保存時に自動で加工する
+     *
+     * return Attribute
+     */
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            // 取得時(Accessor) 取得時タイトルの前に[メモ]をつける
+            get: fn(?string $value) => $value === null ? null : "【メモ】{$value}",
+
+            // 保存時(Mutator) 保存時前後の空白を削除する
+            set: fn(?string $value) => $value === null ? null : trim($value),
+        );
+    }
+
+    /**
+     * 本文から60文字を抜粋を作成
+     *
+     * return Attribute
+     */
+    protected function contentExcerpt(): Attribute
+    {
+        // $attributeはDBから取得した生データ配列
+        return Attribute::make(
+            get: fn($value, array $attribute) =>
+                Str::limit($attribute['content'] ?? '', 60, '...')
+        );
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,16 +18,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::firstOrCreate([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
         // seederクラスはcallメソッドを用いて呼び出す
-        // $this->call(AuthorsTableSeeder::class);
+        $this->call(AuthorsTableSeeder::class);
 
         // // Factoryクラスはcreateメソッドを用いることで作成することができる
-        // Publisher::factory(50)->create();
+        Publisher::factory(50)->create();
 
         // $this->call(
         //     [
