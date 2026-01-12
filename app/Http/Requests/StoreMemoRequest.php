8b125288610b7d037d95d4eac56b944d4a60796a
seyou-U <rikakorink54t@yahoo.co.jp>
2026-01-12 23:41:17 +0900

    [add] 入力値のバリデーションフォームリクエストを追加

---
+++ app/Http/Requests/StoreMemoRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMemoRequest extends FormRequest
+{
+    /**
+     * リクエストに対する権限を設定する
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * バリデーションルールを設定する
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:50'],
+            'content' => ['required', 'text', 'max:255'],
+        ];
+    }
+}
