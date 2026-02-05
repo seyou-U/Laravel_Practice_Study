5a3d3670aecc822141b19b3c8792daba8836352b
seyou-U <rikakorink54t@yahoo.co.jp>
2026-02-05 15:41:18 +0900

    [add] Laravelで作成したAPIをReactの画面で呼び出す実装について簡易的に確認を行うために実装内容を追加

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,21 @@
 
 use App\Http\Actions\PublisherAction;
 use App\Http\Controllers\MemoController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserActionController;
+use App\Http\Controllers\User\LogoutActionController;
 use App\Http\Controllers\User\LoginActionController;
 use App\Http\Controllers\User\RetrieveActionController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-    // ログインを行い、アクセストークンを発行する
+Route::middleware('api')->group(function () {
+    // 認証不要
     Route::post('/users/login', LoginActionController::class);
     Route::post('/publishers', [PublisherAction::class, 'create']);
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+
     // アクセストークンを用いて、認証ユーザーの情報を取得する
     // Route::post('/users/', RetrieveActionController::class)
     //     ->middleware('auth:jwt');
@@ -18,20 +25,24 @@ use Illuminate\Support\Facades\Route;
     // Route::post('/users/{id}', RetrieveActionController::class)
     //     ->middleware('auth:jwt');
 
+    // Laravel Octaneの動作確認用のルート
+    // Octaneが動作している場合、同じPIDが返ってくる
+    Route::get('/octane-check', function () {
+        return response()->json([
+            'pid' => getmypid(),
+            'time' => microtime(true),
+        ]);
+    });
+
+    // --- 認証必須 ---
     Route::middleware('auth:sanctum')->group(function(){
         // 認証ユーザー情報
         Route::get('/user', UserActionController::class);
         // 認証ユーザーの詳細取得
         Route::get('/users/me', RetrieveActionController::class);
+        // ログアウト（アクセストークン無効化）
+        Route::post('/users/logout', LogoutActionController::class);
 
         Route::apiResource('memos', MemoController::class);
     });
-
-// Laravel Octaneの動作確認用のルート
-// Octaneが動作している場合、同じPIDが返ってくる
-Route::get('/octane-check', function () {
-    return response()->json([
-        'pid' => getmypid(),
-        'time' => microtime(true),
-    ]);
 });
